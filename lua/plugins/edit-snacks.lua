21a4f7d80cbab13243595ddd124b5f5e83c3402a
Kaleb Troyer <kaleb.troyer@gmail.com>
2026-01-28 13:20:14 -0600

    added snacks edit

---
+++ lua/plugins/edit-snacks.lua
@@ -0,0 +1,34 @@
+return {
+    "folke/snacks.nvim",
+    opts = {
+        picker = {
+            -- hidden = true,
+            -- ignored = true,
+            -- sources = {
+            --     files = {
+            --         hidden = true,
+            --         ignored = true,
+            --         exclude = {
+            --             "**/.git/**",        -- everything inside .git
+            --             "**/__pycache__/**", -- python bytecode dirs
+            --             "**/*.pyc",
+            --             "**/*.pyo",
+            --             "*.gitignore",
+            --             "*.neoconf.json",
+            --         }
+            --     },
+            -- },
+        },
+        dashboard = {
+            preset = {
+                header = [[
+   ________  _____     ________  ________  ________ 
+  /        \/      \  /        \/    /   \/        \
+ /    /    /       / /    /    /         /    /    /
+/         /       /_/       __/         /         / 
+\___/____/\________/\______/  \___/____/\___/____/  
+]],
+            },
+        },
+    },
+}
